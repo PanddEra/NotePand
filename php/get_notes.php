949e5280c52b7c0cf6f600779fa89df872295cf5
PandEra <antonovmykola2008@gmail.com>
2025-12-29 10:51:36 +0100

    added logic of getting notes from db

--- php/get_notes.php
+++ php/get_notes.php
@@ -1,2 +1,8 @@
 <?php
-// TODO: getting notes from database
+require 'db.php';
+global $pdo;
+
+$stmt = $pdo->query("SELECT * FROM notes ORDER BY created_at DESC");
+$notes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode($notes);
