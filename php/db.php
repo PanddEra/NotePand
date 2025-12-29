9f0fbf15d6b38863496584f6a29829af111d73de
PandEra <antonovmykola2008@gmail.com>
2025-12-29 10:18:26 +0100

    added database connection

--- php/db.php
+++ php/db.php
@@ -1,2 +1,17 @@
 <?php
-// TODO: database connection
+
+$host = "localhost";
+$dbname = "notes_app";
+$user = "root";
+$pass = ""; // usually empty in XAMPP
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $user,
+        $pass
+    );
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
